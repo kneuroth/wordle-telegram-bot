18fe65bbeaf3cf12870e3503e3833c98b9fd3ba0
Kelly <kelly.neuroth@outlook.com>
2023-04-16 13:32:34 -0400

    CHecking http

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ if [ "$ENVIRONMENT" == "prod" ]; then
     echo "Running with https"
     gunicorn --certfile=/etc/letsencrypt/live/wordle-bot.kneubots.com/fullchain.pem  --keyfile=/etc/letsencrypt/live/wordle-bot.kneubots.com/privkey.pem --bind 0.0.0.0:443 app:app
 else
+    echo "Running without https" 
     gunicorn --bind 0.0.0.0:8000 app:app
 fi
 
